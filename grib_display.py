d1a896e38d3c493f628c2bec962af5dda08bd856
chvoong24 <chrsv00ng6@gmail.com>
2025-10-16 14:52:55 -0400

    messing with xarray and cartopy to grpah

--- grib_display.py
+++ grib_display.py
@@ -7,8 +7,32 @@ import cartopy.crs as ccrs
 import numpy as np
 import pandas as pd
 
-ds = xr.load_dataset("nbm_data/nbm_download/nbm_t00z_f001_custom.grib2", engine="cfgrib")
+
+# lat = ds.air.lat.data
+# lon = ds.air.lon.data
+# temp = ds.air.data
+ds = xr.load_dataset("nbm_download/nbm_t18z_f051_custom.grib2", engine="cfgrib")
+def main():
+    # ds = xr.load_dataset("nbm_data/nbm_download/nbm_t00z_f001_custom.grib2", engine="cfgrib")
+    plt.figure()
+
+# if __name__ == "__main__":
+#     # main()
+# plt.pcolormesh(lon, lat, temp[0, :, :]);
+
+# print(ds.coords["longitude"])\
+
+
+lat = ds.latitude.data
+lon = ds.longitude.data
+# temp = ds.data_vars["aptmp"].data
+
+ds.attrs
+ds.data_vars
+ds.coords
+# plt.figure()
+# plt.pcolormesh(lon, lat, temp[:, :]);
 
 
 
-print(ds)
+
