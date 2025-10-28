d38744d25ea71e4a00bd348c4df3adb2f52321ec
chvoong24 <chrsv00ng6@gmail.com>
2025-10-28 15:40:17 -0400

    working on grabbing data

--- nbm_visualizer.py
+++ nbm_visualizer.py
@@ -11,24 +11,25 @@ import pandas as pd
 # lat = ds.air.lat.data
 # lon = ds.air.lon.data
 # temp = ds.air.data
-ds = xr.load_dataset("nbm_data/nbm_download/blend.t00z.qmd.f001.co.grib2", engine="cfgrib")
-def main():
-    # ds = xr.load_dataset("nbm_data/nbm_download/nbm_t00z_f001_custom.grib2", engine="cfgrib")
-    plt.figure()
+ds = xr.load_dataset("nbm_data/nbm_download/nbm_t00z_f001_custom.grib2.part", engine="cfgrib")
+# ds.sel(lat = 52, long = 251.8998, method = "nearest").plot()
 
 # if __name__ == "__main__":
 #     # main()
 # plt.pcolormesh(lon, lat, temp[0, :, :]);
 
 # print(ds.coords["longitude"])\
+# lat = ds.latitude.data
+# lon = ds.longitude.data
+ds
+# ds.sel(time="2025-10-28")
 
 
-lat = ds.latitude.data
-lon = ds.longitude.data
-# temp = ds.data_vars["aptmp"].data
 
-ds.attrs
-ds.data_vars
-ds.coords
+# # temp = ds.data_vars["aptmp"].data
+
+# ds.attrs
+# ds.data_vars
+# ds.coords
 # plt.figure()
 # plt.pcolormesh(lon, lat, temp[:, :]);
