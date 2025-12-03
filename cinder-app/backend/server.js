d40cde62c46c812183500adfb026c6e29f782625
Hawklight1 <ben.mck@outlook.com>
2025-12-03 10:24:23 -0500

    Fixed Node.JS server to bind to 0.0.0.0 instead of localhost (makes it publiclly acessible for frontend)

--- cinder-app/backend/server.js
+++ cinder-app/backend/server.js
@@ -29,7 +29,7 @@ const startServer = async () => {
   try {
     await connectDB();
     const PORT = process.env.PORT || 5050;
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    app.listen(PORT, "0.0.0.0",() => console.log(`Server running on port ${PORT}`));
   } catch (error) {
     console.error("Failed to start server:", error);
   }
